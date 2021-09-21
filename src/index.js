1b158cc61627e3d84da64738cd50affaa556a0b5
J Heaton <jackrobertheaton@gmail.com>
2021-09-21 12:00:32 -0400

    Add hamburger menu btn and animation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import "./styles/style.css"
-import MainInitView from "./modules/loadInitContent";
+import InitView from "./modules/loadInitContent";
 
 
-const loadDisplay = (() => {
+const loadDisplay = () => {
 
     const content = document.getElementById("content")
 
-    const showContent = () => MainInitView(content)
+    const showContent = () => {
+        // Load & Add Nav to DOM + Page
+        InitView.loadNav(content)
+
+    }
 
     return {
         showContent
     }
-})
+}
 
 window.onload = () => loadDisplay().showContent()
--- src/modules/loadInitContent.js
+++ src/modules/loadInitContent.js
@@ -1,9 +1,52 @@
 import Help from "../helper";
-import getNav from "./loadNav";
 
-const MainInitView = ( (content)=> {
-    getNav.loadNav(content)
+// This will be responsible for loading the DOM related elements onto the page
+const InitView = (()=> {
 
-})
+    const loadNav = (content) => {
+        // Create Hamburger Menu
+        const hamburger = (() => {
+            const showHam = () => {
+                const hamBtn = Help.makeEl("div", {
+                    class: "menu-btn-burger"
+                })
+                return Help.makeEl("div", {
+                    class: "menu-btn"
+                }, hamBtn)
+            }
 
-export default MainInitView
+            const animateHam = () => {
+                const hamBtnDom = document.querySelector(".menu-btn")
+                let active = false
+                hamBtnDom.addEventListener("click", () => {
+                    if (!active) {
+                        hamBtnDom.classList.add("open")
+                        active = true
+                    } else {
+                        hamBtnDom.classList.remove("open")
+                        active = false
+                    }
+                })
+            }
+         return {
+                showHam,
+                animateHam
+         }
+        })()
+
+        const navBar = Help.makeEl("div", {
+            class: "nav-bar",
+            id: "nav"
+        }, hamburger.showHam())
+
+        content.appendChild(navBar)
+        hamburger.animateHam()
+    }
+
+    return {
+        loadNav
+    }
+
+})()
+
+export default InitView
